a8bb0710ed876c8d15ebcc7de1903d50f759f7f1
BEQSONA-cmd <tvildiani2001@gmail.com>
2026-02-26 18:44:34 +0100

    updates

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import './global.css';
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-5xl text-red-500 font-bold">Hello</Text>
+      <Text className="text-5xl text-blue-500 font-bold">Hello</Text>
     </View>
   );
 }
